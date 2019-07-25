18674c0e95dcdf8b9415ebbc6b80a97ba4b3130f
Daniel <dfdazac@gmail.com>
2019-07-25 14:39:04 +0200

    Add jobs

---
+++ jobs/data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=data
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:05:00
+#SBATCH --mem=10000M
+#SBATCH --partition=gpu_shared
+
+SRC_DIR=monet
+DATA_DIR=data
+DATASET=circles
+SAMPLES=50000
+
+# Copy data to scratch
+cp -r $HOME/$SRC_DIR/$DATA_DIR $TMPDIR
+cd $TMPDIR/$DATA_DIR
+
+# Run experiment
+source activate pytorch
+srun python -u data.py generate_$DATASET with num_samples=$SAMPLES
+
+cp -r $TMPDIR/$DATA_DIR/$DATASET $HOME/$SRC_DIR/$DATA_DIR
---
+++ jobs/train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=train
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:30:00
+#SBATCH --mem=20000M
+#SBATCH --partition=gpu_shared
+#SBATCH --gres=gpu:1
+
+# circles: ~ 68 steps/second
+
+SRC_DIR=monet
+
+# Copy data to scratch
+cp -r $HOME/$SRC_DIR $TMPDIR
+cd $TMPDIR/$SRC_DIR
+
+# Run experiment
+source activate pytorch
+srun python -u train.py with dataset='circles-small' steps=10000
